764f2a3c1e5a4c138f47237237e1aeb1a442ab9d
ashwini lalithaHuchahanumegowda <ashwini.lh12@gmail>
2022-10-06 12:29:06 -0500

    update tasks dynamically and time blocks added

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,33 @@
 //today's date loaded at the top of the page
 var day = (moment().format("MMMM D, YYYY"));
-    console.log(day)
-
-var currentDate = function(){
     $("#currentDay").text(day);
-}
 
-currentDate();
+    //To update task
+    $("#task").on("click", "p", function(){
+        console.log("<p> was clicked");
+        var text =$(this)
+          .text()
+          .trim();
+        var textInput =$("<textarea>")
+          .addClass("form-control")
+          .val(text);
+    
+        $(this).replaceWith(textInput);
+         textInput.trigger("focus");
+      });
+    
+      // Tasks pending
+    $("#task").on("blur", "textarea", function(){
+      //get the textareas; current value/text
+        var text = $(this)
+          .val()
+          .trim();
+        console.log(text)
+    
+        var taskP = $("<p>")
+          .addClass("m-1")
+          .text(text);
+    
+        // To replace textarea 
+        $(this).replaceWith(taskP);
+      });    
