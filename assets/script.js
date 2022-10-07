2257739c0a947ed5c58273f054074b9d40994ee6
ashwini lalithaHuchahanumegowda <ashwini.lh12@gmail>
2022-10-07 10:48:44 -0500

    tasks saved in local storage

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,50 @@
+var loadTasks = function () {
+    tasks = JSON.parse(localStorage.getItem("tasks"))
+    if (!tasks) {
+        tasks = {};
+    }
+}
 //today's date loaded at the top of the page
 var day = (moment().format("MMMM D, YYYY"));
-    $("#currentDay").text(day);
+$("#currentDay").text(day);
 
-    //To update task
-    $(".task").on("click", "p", function(){
-        console.log("<p> was clicked");
-        var text =$(this)
-          .text()
-          .trim();
-        var textInput =$("<textarea>")
-          .addClass("form-control")
-          .val(text);
-    
-        $(this).replaceWith(textInput);
-         textInput.trigger("focus");
-      });
-    
-      // Tasks pending
-    $(".task").on("blur", "textarea", function(){
-      //get the textareas; current value/text
-        var text = $(this)
-          .val()
-          .trim();
-        console.log(text)
-    
-        var taskP = $("<p>")
-          .addClass("m-1")
-          .text(text);
-    
-        // To replace textarea 
-        $(this).replaceWith(taskP);
-      });    
+//To update task
+$(".task").on("click", "p", function () {
+    console.log("<p> was clicked");
+    var text = $(this)
+        .text()
+        .trim();
+    var textInput = $("<textarea>")
+        .addClass("form-control")
+        .val(text);
 
-      $(".save-btn").on("click", function(){
-        console.log("<save button> was clicked");
-        console.log($(".saveBtn").index(this));
-    });
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+});
+
+// Tasks pending
+$(".task").on("blur", "textarea", function () {
+    //get the textareas; current value/text
+    var text = $(this)
+        .val()
+        .trim();
+    console.log(text)
+
+    var taskP = $("<p>")
+        .addClass("m-1")
+        .text(text);
+
+    // To replace textarea 
+    $(this).replaceWith(taskP);
+});
+
+$(".save-btn").on("click", function () {
+    console.log("<save button> was clicked");
+    var index = $(".saveBtn").index(this);
+    console.log(index)
+    tasks[index] = $(this).parent().find("p").text();
+    console.log(tasks)
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+
+});
